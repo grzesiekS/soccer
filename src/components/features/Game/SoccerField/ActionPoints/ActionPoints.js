762eec50375cd79b5e82a5f76882a71628e9bc67
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2022-02-05 16:05:32 +0100

    Add ball position setters

--- src/components/features/Game/SoccerField/ActionPoints/ActionPoints.js
+++ src/components/features/Game/SoccerField/ActionPoints/ActionPoints.js
@@ -7,14 +7,36 @@ import styles from './ActionPoints.module.scss';
 import {GameContext} from '../../../../../ContextAPI/GameContext';
 
 const ActionPoints = ({ columns }) => {
-  const {ballPositionContext} = useContext(GameContext);
+  const {ballPositionContext, gameMovesContext} = useContext(GameContext);
 
-  const [ballPosition] = ballPositionContext;
+  const [ballPosition, setBallPosition] = ballPositionContext;
+  const [gameMoves, setGameMoves] = gameMovesContext;
 
   const determineBallPosition = (actionPoint) => {
     if(ballPosition[0] === actionPoint[0]
       && ballPosition[1] === actionPoint[1]) {
       return true;
+    }
+    return false;
+  };
+
+  const ballPositionSetter = positionCoordinates => {
+    if((positionCoordinates[0] >= ballPosition[0] - 1
+        && positionCoordinates[0] <= ballPosition[0] + 1)
+      && (positionCoordinates[1] >= ballPosition[1] - 1
+        && positionCoordinates[1] <= ballPosition[1] + 1)
+      && !ballPositionInGameMovesArray(positionCoordinates)) {
+      setBallPosition(positionCoordinates);
+      setGameMoves(prevGameMoves => [...prevGameMoves, [ballPosition, positionCoordinates]]);
+    }
+  };
+
+  const ballPositionInGameMovesArray = positionCoordinates => {
+    for (const move of gameMoves) {
+      if((move[0][0] === ballPosition[0] && move[0][1] === ballPosition[1] && move[1][0] === positionCoordinates[0] && move[1][1] === positionCoordinates[1])
+      || (move[1][0] === ballPosition[0] && move[1][1] === ballPosition[1] && move[0][0] === positionCoordinates[0] && move[0][1] === positionCoordinates[1])) {
+        return true;
+      }
     }
     return false;
   };
@@ -28,6 +50,7 @@ const ActionPoints = ({ columns }) => {
           className={determineBallPosition(column)
             ? clsx(styles.point, styles.ball)
             : styles.point}
+          onClick={() => ballPositionSetter(column)}
         />
       ))}
     </div>
