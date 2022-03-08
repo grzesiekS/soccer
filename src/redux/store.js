003fcc77b79a76bfe134d6f2907125752ba4feb4
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2022-03-08 20:55:27 +0100

    Add basic redux code for app

---
+++ src/redux/store.js
@@ -0,0 +1,41 @@
+import {combineReducers, createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import playersData from './data/players.json';
+
+import globalReducer from './globalRedux';
+
+const initialState = {
+  players: playersData,
+};
+
+// define reducers
+const reducers = {};
+
+// add blank reducers for initial state properties without reducers
+Object.keys(initialState).forEach(item => {
+  if (typeof reducers[item] == 'undefined') {
+    reducers[item] = (statePart = null) => statePart;
+  }
+});
+
+// combine reducers
+const combinedReducers = combineReducers(reducers);
+
+// merge all reducers with globalReducer
+const storeReducer = (state, action) => {
+  const modifiedState = globalReducer(state, action);
+  return combinedReducers(modifiedState, action);
+};
+
+// create store
+const store = createStore(
+  storeReducer,
+  initialState,
+  composeWithDevTools(
+    applyMiddleware(thunk)
+  ),
+);
+
+export default store;
